dfeffe79595384a79510bca7181fdab736384df3
chris w <chris.wessel@me.com>
2019-08-23 14:42:19 -0400

    drafts api

--- src/main/java/com/chris/legends/hof/local/api/repository/DraftRepository.java
+++ src/main/java/com/chris/legends/hof/local/api/repository/DraftRepository.java
@@ -1,6 +1,6 @@
 package com.chris.legends.hof.local.api.repository;
 
-import com.chris.legends.hof.local.api.model.Draft;
+import com.chris.legends.hof.local.api.model.entity.Draft;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface DraftRepository extends PagingAndSortingRepository<Draft, Integer> {
